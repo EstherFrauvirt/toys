2e16abaceb7758fcd51a86f6c5a9bfb6832065f8
EstherFrauvirt <esty2048@gmail.com>
2023-12-04 11:39:26 +0200

    add statics use

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const userRoute=require('./routes/user.routes');
 const toyRoute=require('./routes/toy.routes');
+const path=require('path');
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
 
 
 app.get("/test", (req, res) => {
@@ -10,7 +12,7 @@ app.get("/test", (req, res) => {
 });
 app.use('/api/v1/users',userRoute);
 app.use('/api/v1/toys',toyRoute);
-/* Global error handler */
+
 app.use((error, req, res, next) => {
     console.log(error);
     return res.status(400).send({ msg: error.message });
